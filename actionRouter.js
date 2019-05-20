cdf74dce6fc34ab60293cbd3cca1a93626516963
raberin <rmvaberin@gmail.com>
2019-05-19 20:45:50 -0700

    MVP Complete

--- actionRouter.js
+++ actionRouter.js
@@ -32,4 +32,60 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const action = req.body;
+  if (!action.notes) {
+    res.status(404).json({ message: "Missing action notes" });
+  } else if (!action.description) {
+    res.status(404).json({ message: "Missing action description" });
+  } else if (!action.project_id) {
+    res.status(404).json({ message: "Missing action project id" });
+  } else {
+    try {
+      const addingAction = await actionDb.insert(action);
+      res.status(200).json(addingAction);
+    } catch (err) {
+      res.status(500).json({
+        message: "Error adding action"
+      });
+    }
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(404).json({ message: "missing ID or wrong ID" });
+  } else {
+    try {
+      const deletedAction = await actionDb.remove(id);
+      res.status(204).json(deletedAction);
+    } catch (err) {
+      res.status(500).json({
+        message: "Error deleting action"
+      });
+    }
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(404).json({ message: "missing ID or wrong ID" });
+  } else if (!req.body.notes || !req.body.description || !req.body.project_id) {
+    res
+      .status(404)
+      .json({ message: "Missing name or description or project id" });
+  } else {
+    try {
+      const updatingAction = await actionDb.update(id, req.body);
+      res.status(200).json(updatingAction);
+    } catch (err) {
+      res.status(500).json({
+        message: "Error updating action"
+      });
+    }
+  }
+});
+
 module.exports = router;
